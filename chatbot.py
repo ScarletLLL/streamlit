53e3c5137029eed9982c5494439538e312774f9e
Joann Shen <joannshen@joanns-mbp.home>
2024-03-02 08:25:04 -0500

    test

--- chatbot.py
+++ chatbot.py
@@ -34,7 +34,7 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     #response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    response=agent（messages=st.session_state.messages）
+    response=agent（st.session_state.messages）
     msg = response
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
