828f8cf0b2624cd4f66bc20832a88fa36c092d9a
Joann Shen <joannshen@joanns-mbp.home>
2024-03-01 22:34:08 -0500

    test

--- test.py
+++ test.py
@@ -7,21 +7,20 @@ import seaborn as sns
 from langchain_experimental.agents import create_pandas_dataframe_agent 
 from langchain_openai import OpenAI
 
-st.title("FinGPT Application")
 
+with st.sidebar:
+    OPENAI_API_KEY = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 #setup the api key 
-OPENAI_API_KEY="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"
 os.environ['OPENAI_API_KEY']=OPENAI_API_KEY
+
+
+st.title("FinGPT Assistant")
 
 # Importing the data
 df = pd.read_csv('df_model.csv') 
-# Initializing the agent 
-# We have kept verbose= True. It will print all the intermediate steps during the execution.
-model_name="gpt-3.5-turbo-instruct"
-agent = create_pandas_dataframe_agent(OpenAI(temperature=0,model=model_name), df, verbose=True) 
- # temperature parameter is used to adjust the creativity of the model.When it is set to 0, the model is least prone to hallucination.
-openai = OpenAI(temperature=0.0,model=model_name)  
-openai.model_name # This will print the model being used, 
-                  # by default it uses ‘text-davinci-003’
 
 st.table(df_model)
+
+
+
+
