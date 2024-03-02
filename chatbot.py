21f2b3cdfcd2cb124d4d5920d9facfba906cc1cb
Joann Shen <joannshen@joanns-mbp.home>
2024-03-02 08:23:01 -0500

    test

--- chatbot.py
+++ chatbot.py
@@ -8,6 +8,11 @@ with st.sidebar:
 
 st.title("FinInv Chatbot")
 st.caption("A financial chatbot powered by OpenAI LLM")
+
+# Importing the data
+df_model = pd.read_csv('df_model.csv') 
+
+# Chatbot
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -19,10 +24,17 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
+
+
+    # Initializing the agent 
+    # We have kept verbose= True. It will print all the intermediate steps during the execution.
+    model_name="gpt-3.5-turbo-instruct"
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0,model=model_name), df, verbose=True) 
+    #client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
+    #response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response=agent（messages=st.session_state.messages）
+    msg = response
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
